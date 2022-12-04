0b644fd47a2bc8177e4710b5ded4a95281d05575
Gergely Tusko <gergely.tusko@gmail.com>
2022-12-04 15:39:02 +0100

    Initialize project

---
+++ src/main/kotlin/com/gergelytusko/JourneyCache/JourneyCacheApplication.kt
@@ -0,0 +1,11 @@
+package com.gergelytusko.JourneyCache
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JourneyCacheApplication
+
+fun main(args: Array<String>) {
+	runApplication<JourneyCacheApplication>(*args)
+}
---
+++ src/test/kotlin/com/gergelytusko/JourneyCache/JourneyCacheApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.gergelytusko.JourneyCache
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class JourneyCacheApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
