3d2e67d471e9f9c7222355311f3aaf46da2abe7b
Gergely Tusko <gergely.tusko@gmail.com>
2022-12-06 00:13:17 +0100

    Create API endpoints

--- src/main/kotlin/com/gergelytusko/JourneyCache/JourneyCacheApplication.kt
+++ src/main/kotlin/com/gergelytusko/JourneyCache/JourneyCacheApplication.kt
@@ -2,10 +2,32 @@ package com.gergelytusko.JourneyCache
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
 class JourneyCacheApplication
 
 fun main(args: Array<String>) {
 	runApplication<JourneyCacheApplication>(*args)
+}
+data class Journey(val id: Int,
+				   val userId: Int,
+				   val startLong: Double,
+				   val startLat: Double,
+				   val endLong: Double,
+				   val endLat: Double,
+				   val price: Double);
+
+@RestController
+class JourneyController {
+	@GetMapping("/")
+	fun getIndex() = "Hello world!"
+
+	@GetMapping("/journey/{journey_id}")
+	fun getJourney(@PathVariable journey_id: Int) = "Hello, $journey_id!"
+
+	@GetMapping("/user/{user_id}/journeys")
+	fun getUserJourneys(@PathVariable user_id: Int) = "Hello, $user_id!"
 }
